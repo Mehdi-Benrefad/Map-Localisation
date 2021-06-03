30297e51b2028abf4273b4693c7c68e81850ac8c
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-06-04 00:49:18 +0100

    bug fixed

--- map-location/ViewController.swift
+++ map-location/ViewController.swift
@@ -9,44 +9,34 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MKMapViewDelegate{
     @IBOutlet weak var MyMap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Longitude et latitude [41.004844, 28.976239]
-        let latitude : CLLocationDegrees = 28.976239
-        let longitude : CLLocationDegrees = 41.004844
         
-        //la distance entre les points de tracage
-        let latDelta : CLLocationDegrees = 0.05
-        let longitudeDelta : CLLocationDegrees = 0.05
-        //portee des coordonnees elle assemble "latDelta" et "longitudeDelta" pour tracer le plan de localisation
-        let span : MKCoordinateSpan = MKCoordinateSpan( latitudeDelta: latDelta, longitudeDelta: longitudeDelta )
-        //definir la location a partir de la longitude et la latitude
-        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        //definir la region de la localisation
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
-        //ajouter la region a la carte
-        MyMap.setRegion(region, animated: true)
+        //Longitude et latitude [41.004844, 28.976239]
+        let latitude : CLLocationDegrees = 38.897957
+        let longitude : CLLocationDegrees = -77.036560
         
         //Ajouter des annotation
         let annotation = MKPointAnnotation()
         //Ajout du titre
-        annotation.title = "Sultan Ahmed"
+        annotation.title = "The white house"
         //Ajout du soustitre
-        annotation.subtitle = "Turkey Istanbul"
+        annotation.subtitle = "USA"
         //Ajout de la position
-        annotation.coordinate = location
-        //Ajouter l'annotation a la carte
+        annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         MyMap.addAnnotation(annotation)
+        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        //ajouter la region a la carte
+        MyMap.setRegion(region, animated: true)
         
         //Ajout des Markers [quand on presse lontement su une position de l'ecran une nouvelle annotation s'ajoutes]
          let mylongpress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(gestureRecognizer:)))
         //definition de la duree du clic enfonce
         mylongpress.minimumPressDuration = 2
         MyMap.addGestureRecognizer(mylongpress)
-        
     }
 
   @objc func longPress(gestureRecognizer: UIGestureRecognizer){
@@ -59,6 +49,6 @@ class ViewController: UIViewController {
             MyMap.addAnnotation(annotation)
        }
 
-
+     
 }
 
