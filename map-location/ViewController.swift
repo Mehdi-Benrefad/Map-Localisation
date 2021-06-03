64bc70f80a283871b945417f07865f6699e4cd79
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-06-03 21:21:23 +0100

    Initial Commit

---
+++ map-location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map-location
+//
+//  Created by Mehdi Benrefad on 03/06/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
